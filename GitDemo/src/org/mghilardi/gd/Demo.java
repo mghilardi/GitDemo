fd97052856b574636b2d2bed1417ab31b0cc2cf0
mghilardi <mauricio.ghilardi@gmail.com>
2015-03-20 19:01:23 +0000

    Add simple output change to the main method.

--- GitDemo/src/org/mghilardi/gd/Demo.java
+++ GitDemo/src/org/mghilardi/gd/Demo.java
@@ -3,7 +3,8 @@ package org.mghilardi.gd;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Git Demo");
+		System.out.println("Git Demo commit 1");
+		System.out.println("Git Demo commit 2");
 	}
 
 }
