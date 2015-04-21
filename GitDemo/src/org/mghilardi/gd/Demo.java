4e3db9e366dc05736bb15d66d73a810c1d5fae19
mghilardi <mauricio.ghilardi@gmail.com>
2015-04-21 16:14:15 +0100

    Git Demo commit 3: after clone from git repository

--- GitDemo/src/org/mghilardi/gd/Demo.java
+++ GitDemo/src/org/mghilardi/gd/Demo.java
@@ -1,10 +1,13 @@
 package org.mghilardi.gd;
 
+import static java.lang.System.out;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Git Demo commit 1");
-		System.out.println("Git Demo commit 2");
+		out.println("Git Demo commit 1");
+		out.println("Git Demo commit 2");
+		out.println("Git Demo commit 3: after clone from git repository");
 	}
 
 }
